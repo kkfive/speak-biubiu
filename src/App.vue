29d56adc2f1c45c9d218f0d11dde20a77fab7cec
DreamyTZK <dreamytzk@outlook.com>
2022-02-24 18:44:46 +0800

    ✨ feat: 添加子组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="m-0 mx-auto" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <h1 class="text-3xl font-bold text-center underline">Hello world!</h1>
+  <n-config-provider :theme="theme">
+    <index-view></index-view>
+  </n-config-provider>
 </template>
 
-<style></style>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import indexView from './views/indexView.vue'
+import { NConfigProvider } from 'naive-ui'
+import type { GlobalTheme } from 'naive-ui'
+
+const theme = ref<GlobalTheme | null>(null)
+</script>
+
+<style>
+body {
+  background: linear-gradient(
+    90deg,
+    rgba(247, 149, 51, 0.1),
+    rgba(243, 112, 85, 0.1) 15%,
+    rgba(239, 78, 123, 0.1) 30%,
+    rgba(161, 102, 171, 0.1) 44%,
+    rgba(80, 115, 184, 0.1) 58%,
+    rgba(16, 152, 173, 0.1) 72%,
+    rgba(7, 179, 155, 0.1) 86%,
+    rgba(109, 186, 130, 0.1)
+  );
+}
+</style>
---
+++ src/views/indexView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="max-w-5xl px-10 py-10 bg-white rounded-lg shadow-xl"
+    style="margin: 0 auto"
+  >
+    <div class="f-10">123</div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+
+<style scoped></style>
