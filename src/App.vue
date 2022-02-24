ae6ef8110fc7ab1fcdf4627879359fc20fad0f63
DreamyTZK <dreamytzk@outlook.com>
2022-02-24 14:35:35 +0800

    🎉 init: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" class="m-0 mx-auto" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1 class="text-3xl font-bold text-center underline">Hello world!</h1>
+</template>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-center">{{ msg }}</h1>
+
+  <p class="text-center">
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p class="text-center">See <code>README.md</code> for more information.</p>
+
+  <p class="text-center">
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button class="m-0 mx-auto" type="button" @click="count++">
+    count is: {{ count }}
+  </button>
+  <p class="text-center">
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
